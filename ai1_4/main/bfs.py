f6f1d19dee10cfc076d121f11c4aad5c4ddb7d44
Robert David Hernandez <rherna57@mail.depaul.edu>
2020-02-11 21:46:13 -0600

    get basic unit testing setup in python

--- ai1_4/main/bfs.py
+++ ai1_4/main/bfs.py
@@ -2,5 +2,9 @@ def main():
     None
 
 
+def foo():
+    return 1
+
+
 if __name__ == "__main__":
     main()
---
+++ ai1_4/test/test_main.py
@@ -0,0 +1,18 @@
+import unittest
+
+from main import bfs
+
+class MyTestCase(unittest.TestCase):
+
+    def test_foo(self):
+        self.assertEqual(1, bfs.foo())
+
+    def test_normal_input_expect_normal_output(self):
+        input = "1 0 2 4 5 7 3 8 9 6 11 12 13 10 14 15"
+        expected_output = "RDLDDRR"
+        actual_output = bfs.foo()
+        self.assertEqual(expected_output, actual_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
