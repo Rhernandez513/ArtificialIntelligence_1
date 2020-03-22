f0a66060d2c6ed995ebd15c4e4c7dcf014a5eaf0
Robert David Hernandez <rherna57@mail.depaul.edu>
2020-03-21 22:35:18 -0500

    add some runtime improvements at the cost of optimality and run some extra test cases

--- src/com/company/IDAStar.java
+++ src/com/company/IDAStar.java
@@ -1,22 +1,39 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.Set;
 
 public class IDAStar {
   public static void main(String[] args) {
+    System.out.println("Use Both Heuristics or only ManhattanDistance? y/n");
+    Scanner s = new Scanner(System.in);
+    Character i = s.nextLine().charAt(0);
 
-    final String input = (args.length > 0) ? args[0] : "1 0 2 4 5 7 3 8 9 6 11 12 13 10 14 15";
+    // one iter
+//    final String input = (args.length > 0) ? args[0] : "1 0 2 4 5 7 3 8 9 6 11 12 13 10 14 15";
+    // two iter
+    final String input = (args.length > 0) ? args[0] : "1 2 3 4 5 10 6 0 9 7 11 8 13 14 15 12";
+    // ONLY USE MANHATTAN WITH THESE, COMMENT OUT LINE 30 TO DISABLE Misplaced Tiles
+    // five iter
+//    final String input = (args.length > 0) ? args[0] : "5 2 4 8 10 3 11 14 6 0 9 12 13 1 15 7";
+    // nine iter
+//    final String input = (args.length > 0) ? args[0] : "5 2 4 8 10 0 3 14 13 6 11 12 1 15 9 7";
 
     final int[][] initialState = Util.getStateFromSting(input);
     final int[][] goalState = Util.goalState;
 
     Problem problem = new Problem(initialState, goalState);
-    run(problem, false);
+    if(i.equals('y')) {
+      run(problem, false);
+    }
     run(problem, true);
   }
 
@@ -26,7 +43,7 @@ public class IDAStar {
     final long startTime = System.nanoTime();
     List<Actions> solutionSequence = null;
     try {
-      solutionSequence = IDAStarSearch(problem, isManhattan);
+      solutionSequence = IDAStarSearch(problem, isManhattan, Util.calculateManhattanDistance(problem.initialState));
     } catch (Exception e) {
       System.out.println(e.getMessage());
       e.printStackTrace();
@@ -37,7 +54,7 @@ public class IDAStar {
       System.exit(1);
     }
 
-    Util.printResult(startTime, problem, solutionSequence);
+    Util.printResult(startTime, problem, solutionSequence, isManhattan);
   }
 
   // The key addition of Depth Limiting is that on each iteration, the depth limit is increased
@@ -46,19 +63,25 @@ public class IDAStar {
   // f(n) = g(n) + h(n)
   // g(n) = step cost from node to goal
   // h(n) = heuristic of node (manhattan || misplaced)
-  static List<Actions> IDAStarSearch(Problem problem, boolean isManhattan) throws Exception {
+  static List<Actions> IDAStarSearch(Problem problem, boolean isManhattan, int initialDepth) throws Exception {
     // I've ran a test case that ran the GC out of memory with only a depth limit of 100
     // but if you want you can try  Integer.MAX_VALUE
-    int infinity = 100;
-    for (int depth = 1; depth < infinity; /**/ ) {
+    int infinity = Integer.MAX_VALUE;
+    System.out.println(String.format("Using %d for Infinity", infinity));
+    System.out.print(String.format("Running AStar with depth of: "));
+    for (int depth = initialDepth; depth < infinity; /**/ ) {
+      System.out.print(String.format("%d..", depth));
       Object result = depthLimitedAStarSearch(problem, isManhattan, depth);
       if (result == null) { return null; }
       if(result instanceof Integer) {
         depth += (Integer) result;
         continue;
       }
-      if(result instanceof List) { return (List) result; }
-      throw new Exception("Something unexpected happened");
+      if(result instanceof List) {
+        System.out.println();
+        return (List) result;
+      }
+      throw new Exception("\nSomething unexpected happened");
     }
     return null;
   }
@@ -84,19 +107,12 @@ public class IDAStar {
       for (Actions action : problem.actions(node.state)) {
         Node child = node.childNode(problem, action, isManhattanEnabled);
         ++problem.expandedCount; // for metric tracking, not part of algo
+        // Check for goal state at time of generation
+        if (problem.goalTest(node.state)) { return node.solution(); }
         // Check if Depth-Limit has been exceeded
         if(node.heuristicPathCost() > limit) { return node.heuristicPathCost(); }
         if (!explored.contains(child.state) && !frontier.contains(child)) {
           frontier.add(child);
-        } else if (frontier.contains(child)) {
-          // else if child.STATE is in frontier with higher PATH-COST then
-          for (Node n : frontier) {
-            if (n.equals(child) && (n.pathCost > child.pathCost)) {
-              frontier.remove(n);
-              frontier.add(child);
-              break;
-            }
-          }
         }
       }
     }
